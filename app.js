b489bba3116416c3f41697821cfa9a5e833378d8
Winston Tang <winstontang@Winstons-MBP-16.attlocal.net>
2023-03-16 17:12:08 -0700

    ft: minor change to output

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
-  res.send("Hello World");
+  res.send("Hello World!");
 });
 
 app.use("/api", routes);
